a761328d7930ebe07d5f5b65f7ec6eb1c419a7c9
Mitch Aplin <mitchaplin@gmail.com>
2022-08-31 23:25:25 -0500

    Update portfolio image

--- components/resume/ResumeHeader.tsx
+++ components/resume/ResumeHeader.tsx
@@ -1,9 +1,7 @@
 export const ResumeHeader = () => {
   return (
-    <div
-      className={"py-12 sm:py-20 w-full text-center relative"}
-    >
-      <h1 className="text-3xl sm:text-4xl inline-block w-auto mx-auto mb-8 relative">
+    <div className={"py-12 sm:py-20 w-full text-center relative"}>
+      <h1 className="text-5xl sm:text-6xl inline-block w-auto mx-auto mb-8 relative">
         Resume
       </h1>
     </div>
