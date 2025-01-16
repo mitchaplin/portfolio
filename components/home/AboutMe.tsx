8fafbf1e0d69595d2a84af1494fbd0f1ed8a5b5c
Mitch Aplin <mitchaplin@gmail.com>
2025-01-15 23:53:55 -0600

    Update about me section

--- components/home/AboutMe.tsx
+++ components/home/AboutMe.tsx
@@ -10,20 +10,18 @@ export const AboutMe = () => {
       </h2>
       <div className="relative w-full mx-auto md:mx-none gap-x-8 l:gap-y-0 gap-y-12 sm:gap-8 md:gap-12 grid-cols-3 sm:grid-cols-6 text-left place-content-center">
         <p className="text-2xl lg:ml-[32rem] lg:mr-[6rem] lg:mt-[-2rem]">
-          Proven Software Engineer with 7+ years of development experience. As a
-          dedicated problem solver, I display an expert skill across the stack
-          and supporting frameworks that solve real world problems through code.
-          Utilizing the vast array of experience at Singlewire Software, I have
-          attained hands-on experience in an industry standard development
-          process, and have seen several mission critical projects through from
-          inception to deployment.
+          Experienced Software Engineer with 9+ years in full-stack development,
+          excelling at solving real-world problems through code. At Singlewire
+          Software, I have hands-on experience in industry-standard processes
+          and successfully delivered mission-critical projects from inception to
+          deployment.
         </p>
         <br />
         <div className="m-8" />
         <p className="text-2xl lg:ml-[32rem] lg:mr-[6rem] lg:mt-[-2rem]">
           I am highly motivated in expanding my horizons day by day for better
-          performance, testing and user experience. <i>Lifetime Learning</i> is
-          a mantra I deeply subscribe to.
+          performance, testing and UX. <i>Lifetime Learning</i> is a mantra I
+          passionately subscribe to.
         </p>
       </div>
     </div>
