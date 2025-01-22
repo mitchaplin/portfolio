34b09990f2a178a0378f37afacc2866ae7b93cdf
Mitch Aplin <mitchaplin@gmail.com>
2025-01-22 16:53:07 -0600

    Add animation to projects area - Remove yarn in favor of pnpm

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -5,7 +5,7 @@ import { kebabCase } from "../../data/utils";
 export const ProjectCard = ({ project }: any) => {
   return (
     <div
-      className="max-w-sm flex flex-col projects-center md:projects-start md:justify-center"
+      className="projectElem max-w-sm flex flex-col projects-center md:projects-start md:justify-center"
       key={project.id}
     >
       <a
@@ -27,7 +27,8 @@ export const ProjectCard = ({ project }: any) => {
             {project.role == "contributor" ? (
               <span className="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300 h-5">
                 {project.role}
-              </span> ) : (
+              </span>
+            ) : (
               <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300 h-5">
                 {project.role}
               </span>
