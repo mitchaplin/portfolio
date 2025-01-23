a2faecfd78768f461ed30db70878d638d3b7b49e
Mitch Aplin <mitchaplin@gmail.com>
2025-01-22 21:30:53 -0600

    Add shared client component

---
+++ data/shared.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const ClientOnly: React.FC<Props> = ({ children, ...delegated }) => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
+
+  return <div {...delegated}>{children}</div>;
+};
